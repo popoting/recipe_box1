3a67705eb7d56bce2cb6b75f69c7d1cd4421874b
你的名字 <email@xxx.xxx>
2017-04-05 17:53:10 +0800

    added cocoon

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -40,8 +40,8 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :decription, :image, ingredients_attributes: [:id, :name, :destroy], directions_attributes: [:id, :step, :_destroy])
-  end
+		params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+	end
 
   def find_recipe
     @recipe = Recipe.find(params[:id])
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,13 @@
 class Recipe < ApplicationRecord
-  has_many :ingredients
-  has_many :directions
-  accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
-  accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
+  has_many :ingredients, inverse_of: :recipe
+  has_many :directions, inverse_of: :recipe
+  accepts_nested_attributes_for :ingredients,
+	reject_if: proc { |attributes| attributes['name'].blank? },
+	allow_destroy: true
+ 	accepts_nested_attributes_for :directions,
+  reject_if: proc { |attributes| attributes['step'].blank? },
+	allow_destroy: true
+
   validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
